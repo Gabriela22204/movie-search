5a8ea46a8e8f6f6c3941da82c50e1c9ba6206815
Gabriela22204 <gabimiho.mk@gmail.com>
2024-11-07 20:42:14 -0300

    added 2 p element and 'a' element

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,23 @@
       <h1>Entrar</h1>
       <input v-model="userLogin" type="text" placeholder="Email ou número de celular" @input="userLogin"/>
       <input v-model="Password" type="password" placeholder="Senha" @input="Password"/>
-
+      
       <button></button>
+      <p>OU</p>
+      <button></button>
+      <a href="https:www.netflix.com/LoginHelp">Esqueceu a senha?</a>
+      <p>Lembre-se de mim</p>
+      <p>Novo por aqui?</p>
+      <a href="https:www.netflix.com">Assine agora</a>
+      <p>Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô.</p>
+
+      <p>As informações recolhidas pelo Google reCAPTCHA estão sujeitas à <a href="https://policies.google.com/privacy">Política de Privacidade</a> e <a href="https://policies.google.com/terms">Termos de Uso</a>, e são usadas para oferecer, manter e melhorar o serviço reCAPTCHA e por questões de segurança (não são usadas para exibir anúncios personalizados pelo Google).</p>
     </div>
   </div>
 </template>
 
 <script>
+
 
 </script>
 
