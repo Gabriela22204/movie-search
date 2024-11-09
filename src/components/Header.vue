1065d910eeac9273364e95ba4c7279ed57cbc6a5
Gabriela22204 <gabimiho.mk@gmail.com>
2024-11-09 12:58:53 -0300

    moved logo.png to public folder and created header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="header-container">
+      <div data-layout="wrapper" class="header-container-element">
+        <div data-layout="item" class="header-item">
+          <a role="link" class="" href="/">
+            <img src="/icons/site.logo.png" class="header-logo" alt="Netflix Logo">
+            
+            <span class="link-netflix-logo-header">Netflix</span>
+          </a>
+        </div>
+
+      </div>
+    </header>
+</template>
