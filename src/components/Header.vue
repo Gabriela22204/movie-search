16b4b9eb03fd6939ae2c48dfaa53b805a3c09ccf
Gabriela22204 <gabimiho.mk@gmail.com>
2024-11-09 13:01:42 -0300

    added style scoped at header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,32 @@
       <div data-layout="wrapper" class="header-container-element">
         <div data-layout="item" class="header-item">
           <a role="link" class="" href="/">
-            <img src="/icons/site.logo.png" class="header-logo" alt="Netflix Logo">
-            
-            <span class="link-netflix-logo-header">Netflix</span>
+            <img src="/public/icons/site.logo.png" class="header-logo" alt="Netflix Logo">
+            <h1>Netflix</h1>
           </a>
         </div>
 
       </div>
     </header>
 </template>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: #f8f9fa;
+}
+
+.logo {
+  width: 100px; 
+  height: auto;  
+  margin-right: 20px;  /* Space between logo and title */
+}
+
+h1 {
+  font-size: 24px;
+  color: #333;
+  margin: 0;
+}
+</style>
