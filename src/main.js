92ef31044fd2072304ae81ec793663771c0cbc27
Gabriela22204 <gabimiho.mk@gmail.com>
2024-11-10 22:12:53 -0300

    added components from primevue at Main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Checkbox from 'primevue/checkbox';
+import Card from 'primevue/card';
+import 'primevue/resources/themes/saga-blue/theme.css'; 
+import 'primevue/resources/primevue.min.css';  // Estilos globais
+import 'primeicons/primeicons.css';  
+import VeeValidate from 'vee-validate'; 
+
+createApp(App)
+.use(PrimeVue)
+.use(VeeValidate)
+.component('Button', Button)
+.component('InputText', InputText)
+.component('Checkbox', Checkbox)
+.component('Card', Card)
+.mount('#app')
